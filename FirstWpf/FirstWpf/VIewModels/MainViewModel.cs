f96a8d669ec3d1c821b57ffcce5c271ebb6f90eb
wi15b004 <wi15b004@technikum-wien.at>
2016-11-10 14:12:41 +0100

    wpf project fe

---
+++ FirstWpf/FirstWpf/VIewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWpf.VIewModels
+{
+    public class MainViewModel
+    {
+        private double price=100;
+        private string title = "DemoTitle";
+        private int amount;
+
+        #region PROPERTIES
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        #endregion 
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
