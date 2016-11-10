0eb3f73a84fa8d6ef0cf2edf1fdbfb6e737b3c43
wi15b004 <wi15b004@technikum-wien.at>
2016-11-10 14:04:22 +0100

    erster wpf versuch

---
+++ WpfApplication1_VL/WpfApplication1_VL/ViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1_VL
+{
+    class ViewModels
+    {
+        public string title;
+
+        #region properties
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+                
+        }
+
+        #endregion properties
+
+    }
+}
