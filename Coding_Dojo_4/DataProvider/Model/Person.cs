9ce48c8a470a3fdd737cf79d378c7b1a640aed5b
wi15b004 <wi15b004@technikum-wien.at>
2016-12-09 17:02:29 +0100

    zwischenstand - perston fertig erstellt und xml überarbeitet

--- Coding_Dojo_4/DataProvider/Model/Person.cs
+++ Coding_Dojo_4/DataProvider/Model/Person.cs
@@ -8,5 +8,8 @@ namespace DataProvider.Model
 {
     class Person
     {
+
+
+
     }
 }
--- Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
+++ Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
@@ -8,5 +8,69 @@ namespace WpfApplication1.ViewModel
 {
     class PersonVM
     {
+        private string firstname;
+        private string lastname;
+        private int ssn;
+        private DateTime birthdate = new DateTime();
+
+        public PersonVM(string firstname, string lastname, int ssn, DateTime birthdate)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.ssn = ssn;
+            this.birthdate = birthdate;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public int Ssn
+        {
+            get
+            {
+                return ssn;
+            }
+
+            set
+            {
+                ssn = value;
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get
+            {
+                return birthdate;
+            }
+
+            set
+            {
+                birthdate = value;
+            }
+        }
     }
 }
