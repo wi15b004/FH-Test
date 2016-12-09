e3d051d9e3c631469cf4581c588d5c42184bf494
wi15b004 <wi15b004@technikum-wien.at>
2016-12-09 17:59:23 +0100

    person public nau and personvm created

--- Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
+++ Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
@@ -1,4 +1,5 @@
 ﻿using DataProvider.Model;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,59 @@ using System.Threading.Tasks;
 
 namespace WpfApplication1.ViewModel
 {
-    class PersonVM
+    public class PersonVM : ViewModelBase
     {
         private Person per;
 
+        public string Firstname
+        {
+            get { return per.Firstname; }
+            set
+            {
+                per.Firstname = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Lastname
+        {
+            get { return per.Lastname; }
+            set
+            {
+                per.Lastname = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int Ssn
+        {
+            get { return per.Ssn; }
+            set
+            {
+                per.Ssn = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get { return per.Birthdate; }
+            set
+            {
+                per.Birthdate = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public PersonVM(string firstname, string lastname, int ssn, DateTime birthdate)
+        {
+            per = new Person(firstname, lastname, ssn, birthdate);
+        }
+
+        public Person GetPerson()
+        {
+            return per;
+        }
 
     }
 }
