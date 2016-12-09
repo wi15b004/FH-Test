54ff00bc52562123409c442bfc22635ba2055f3e
wi15b004 <wi15b004@technikum-wien.at>
2016-12-09 17:38:21 +0100

    changes

--- Coding_Dojo_4/DataProvider/Model/Person.cs
+++ Coding_Dojo_4/DataProvider/Model/Person.cs
@@ -8,8 +8,70 @@ namespace DataProvider.Model
 {
     class Person
     {
+        private string firstname;
+        private string lastname;
+        private int ssn;
+        private DateTime birthdate = new DateTime();
 
+        public Person(string firstname, string lastname, int ssn, DateTime birthdate)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.ssn = ssn;
+            this.birthdate = birthdate;
+        }
 
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public int Ssn
+        {
+            get
+            {
+                return ssn;
+            }
+
+            set
+            {
+                ssn = value;
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get
+            {
+                return birthdate;
+            }
+
+            set
+            {
+                birthdate = value;
+            }
+        }
 
     }
 }
--- Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
+++ Coding_Dojo_4/WpfApplication1/ViewModel/PersonVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,69 +9,8 @@ namespace WpfApplication1.ViewModel
 {
     class PersonVM
     {
-        private string firstname;
-        private string lastname;
-        private int ssn;
-        private DateTime birthdate = new DateTime();
+       // private Person per;
 
-        public PersonVM(string firstname, string lastname, int ssn, DateTime birthdate)
-        {
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.ssn = ssn;
-            this.birthdate = birthdate;
-        }
 
-        public string Firstname
-        {
-            get
-            {
-                return firstname;
-            }
-
-            set
-            {
-                firstname = value;
-            }
-        }
-
-        public string Lastname
-        {
-            get
-            {
-                return lastname;
-            }
-
-            set
-            {
-                lastname = value;
-            }
-        }
-
-        public int Ssn
-        {
-            get
-            {
-                return ssn;
-            }
-
-            set
-            {
-                ssn = value;
-            }
-        }
-
-        public DateTime Birthdate
-        {
-            get
-            {
-                return birthdate;
-            }
-
-            set
-            {
-                birthdate = value;
-            }
-        }
     }
 }
