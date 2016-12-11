537941ae7f3c987a891d668b4690c84b03447ee1
wi15b004 <wi15b004@technikum-wien.at>
2016-12-11 13:26:06 +0100

    .

--- Dojo3/Dojo3/ViewModel/MainViewModel.cs
+++ Dojo3/Dojo3/ViewModel/MainViewModel.cs
@@ -1,4 +1,9 @@
+using CodingDojo4DataLib;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
 
 namespace Dojo3.ViewModel
 {
@@ -22,24 +27,119 @@ namespace Dojo3.ViewModel
 
         private const string showAll = "Show All";
         private List<StockEntry> stock;
-        private ObservableCollection<StockEntryVm> saleitems = new ObservableCollection<StockEntryVm>(); //Vollständige Produktliste (Backup)
-        private ObservableCollection<StockEntryVm> filteredSaleitems = new ObservableCollection<StockEntryVm>(); //Gefilterte Produktliste
+        private ObservableCollection<StockEntryViewModel> saleitems = new ObservableCollection<StockEntryViewModel>(); //Vollständige Produktliste (Backup)
+        private ObservableCollection<StockEntryViewModel> filteredSaleitems = new ObservableCollection<StockEntryViewModel>(); //Gefilterte Produktliste
         private string selectedSearchItem; //Auswahl Combobox
         private ObservableCollection<string> filteredList = new ObservableCollection<string>(); //Inhalt Combobox (Produktliste + ShowAll)
-        private StockEntryVm selectedSalesItem; //Auswahl Datagrid
+        private StockEntryViewModel selectedSalesItem; //Auswahl Datagrid
         public RelayCommand SearchBtn { get; set; }
         public RelayCommand ClearBtn { get; set; }
 
+        #region Properties
+        public ObservableCollection<string> FilteredList
+        {
+            get { return filteredList; }
+            set
+            {
+                filteredList = value;
+            }
+        }
+
+        public ObservableCollection<StockEntryViewModel> Saleitems
+        {
+            get { return saleitems; }
+            set { saleitems = value; }
+        }
+
+        public ObservableCollection<StockEntryViewModel> FilteredSaleitems
+        {
+            get { return filteredSaleitems; }
+            set { filteredSaleitems = value; }
+        }
+
+        public string SelectedSearchItem
+        {
+            get
+            {
+                return selectedSearchItem;
+            }
+            set
+            {
+                selectedSearchItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public StockEntryViewModel SelectedSalesItem
+        {
+            get
+            {
+                return selectedSalesItem;
+            }
+
+            set
+            {
+                selectedSalesItem = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            LoadData();
+            SearchBtn = new RelayCommand(SearchBtnClicked);
+            ClearBtn = new RelayCommand(ClearBtnClicked);
         }
+
+        public void LoadData()
+        {
+            SampleManager manager = new SampleManager();
+            stock = manager.CurrentStock.OnStock;
+            filteredList.Add(showAll);
+
+            foreach (var item in manager.CurrentStock.OnStock)
+            {
+                saleitems.Add(new StockEntryViewModel(item));
+                filteredSaleitems.Add(new StockEntryViewModel(item));
+                filteredList.Add(new StockEntryViewModel(item).Name);
+            }
+        }
+
+        private void ClearBtnClicked()
+        {
+            DeleteData(SelectedSalesItem);
+        }
+
+
+        private void SearchBtnClicked()
+        {
+            FilterData(selectedSearchItem);
+        }
+
+        private void FilterData(string selection)
+        {
+            FilteredSaleitems.Clear();
+            foreach (var item in Saleitems)
+            {
+                if (item.Name.Equals(selection) || selection == showAll)
+                {
+                    FilteredSaleitems.Add(item);
+                }
+            }
+        }
+
+        private void DeleteData(StockEntryViewModel selection)
+        {
+            FilteredSaleitems.Remove(selection);
+            Saleitems.Remove(selection);
+            FilteredList.Clear();
+            FilteredList.Add(showAll);
+            foreach (var item in Saleitems)
+            {
+                FilteredList.Add(item.Name);
+            }
+        }
+
     }
 }
