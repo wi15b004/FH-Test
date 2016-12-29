1b1bae38b994230fb0a2c33d2c06f13c3917412a
wi15b004 <wi15b004@technikum-wien.at>
2016-12-29 10:51:56 +0100

    added images und itemvm

--- Coding_Dojo_5/Coding_Dojo_5/Client.cs
+++ Coding_Dojo_5/Coding_Dojo_5/Client.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,58 @@ namespace Coding_Dojo_5
 {
     class Client
     {
+        byte[] buffer = new byte[512];
+        Socket clientsocket;
+        Action<string> MessageInformer;
+        Action AbortInformer;
+
+        public Client(String ip, int port, Action<string> messageInformer, Action abortInformer)
+        {
+            try
+            {
+                this.AbortInformer = abortInformer;
+                this.MessageInformer = messageInformer;
+                TcpClient client = new TcpClient();
+                client.Connect(IPAddress.Parse(ip), port);
+                clientsocket = client.Client;
+                StartReceiving();
+            }
+            catch (Exception)
+            {
+                messageInformer("Server error");
+                AbortInformer();
+            }
+        }
+
+        private void StartReceiving()
+        {
+            Task.Factory.StartNew(Receive);
+        }
+
+        private void Receive()
+        {
+            string message = "";
+            while (!message.Equals("@quit"))
+            {
+                int length = clientsocket.Receive(buffer);
+                message = Encoding.UTF8.GetString(buffer, 0, length);
+                MessageInformer(message);
+            }
+            Close();
+        }
+
+        public void Send(string message)
+        {
+            if(clientsocket!=null)
+            {
+                clientsocket.Send(Encoding.UTF8.GetBytes(message));
+            }
+        }
+
+        private void Close()
+        {
+            clientsocket.Close();
+            AbortInformer();
+        }
     }
 }
