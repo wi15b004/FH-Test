dd599c3dbceadb91187b5b2f90b8ae290fe4bc3e
wi15b004 <wi15b004@technikum-wien.at>
2017-01-11 11:18:33 +0100

    init

--- CodingDojo6/CodingDojo6/ViewModel/MainViewModel.cs
+++ CodingDojo6/CodingDojo6/ViewModel/MainViewModel.cs
@@ -1,4 +1,8 @@
 using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using System;
+using System.Windows.Media.Imaging;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace CodingDojo6.ViewModel
 {
@@ -19,16 +23,51 @@ namespace CodingDojo6.ViewModel
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
+
+        private ItemVM currentItem;
+        public ObservableCollection<ItemVM> Items { get; set; }
+        public ObservableCollection<ItemVM> Cart { get; set; }
+        private RelayCommand<ItemVM> buyBtnCmd;
+
+        public ItemVM CurrentItem
+        {
+            get { return currentItem; }
+            set { currentItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand<ItemVM> BuyBtnCommnd
+        {
+            get { return buyBtnCmd; }
+            set { buyBtnCmd = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            Cart = new ObservableCollection<ItemVM>();
+            BuyBtnCommnd = new RelayCommand<ItemVM>((t) =>
+            {
+                Cart.Add(t);
+            }, (t) => { return true; });
+            Items = new ObservableCollection<ItemVM>();
+            GenerateData();
+        }
+
+        private void GenerateData()
+        {
+            Items.Add(new ItemVM("Lego", new BitmapImage(new Uri("Images/lego_porsche.png", UriKind.Relative)), "-"));
+            Items.Add(new ItemVM("Playmobil", new BitmapImage(new Uri("Images/playmobil_auto.jpg", UriKind.Relative)), "-"));
+
+            Items[Items.Count - 2].AddItem(new ItemVM("Bagger", new BitmapImage(new Uri("Images/lego_bagger.png", UriKind.Relative)), "10"));
+            Items[Items.Count - 2].AddItem(new ItemVM("Kran", new BitmapImage(new Uri("Images/lego_kran.png", UriKind.Relative)), "12"));
+            Items[Items.Count - 2].AddItem(new ItemVM("Radlader", new BitmapImage(new Uri("Images/lego_radlader.png", UriKind.Relative)), "14"));
+
+            Items[Items.Count - 1].AddItem(new ItemVM("Bad", new BitmapImage(new Uri("Images/playmobil_bad.jpg", UriKind.Relative)), "2"));
+            Items[Items.Count - 1].AddItem(new ItemVM("Camper", new BitmapImage(new Uri("Images/playmobil_camper.jpg", UriKind.Relative)), "3"));
+            Items[Items.Count - 1].AddItem(new ItemVM("Spielplatz", new BitmapImage(new Uri("Images/playmobil_spielplatz.jpg", UriKind.Relative)), "4"));
         }
     }
 }
